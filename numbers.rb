05e8c503d56de32db99622c9c7aa8f45b5a9bcf2
joekennerly <joseph.kennerly@gmail.com>
2019-11-29 18:43:44 -0600

    basix with numbers

---
+++ numbers.rb
@@ -0,0 +1,37 @@
+# Basic arithmatic
+puts 5 + (9 - 1) * 3 / 2
+
+# Exponential (2^3)
+puts 2**3
+
+# Modulus
+puts 20 % 2
+
+# Convert to string
+num = -20
+puts ("my fav num is " + num.to_s)
+
+# Give absolute value of num (if negative)
+puts num.abs()
+
+# Round the number up or down
+num = 20.678
+puts num.round()
+
+# Round up
+puts num.ceil()
+
+# Round down
+puts num.floor()
+
+# Get square root
+puts Math.sqrt(36)
+
+# Logrythmic Function
+puts Math.log(2)
+
+# NOTE: using two integers will always return an integer
+puts 10 / 7
+
+# Using an integer and a floating point will return a float
+puts 10 / 7.0
