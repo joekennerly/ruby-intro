fdba77a81b94dff4c4d68cd49854579cf550e60c
joekennerly <joseph.kennerly@gmail.com>
2019-11-30 08:53:14 -0600

    better calc

--- calculator.rb
+++ calculator.rb
@@ -8,7 +8,6 @@ puts "string: #{(num1 + num2)}"
 
 # Convert the strings to integers
 puts "integer: #{(num1.to_i + num2.to_i)}"
-# ??? is .to_i a method? why no parenthesis???
 
 # Convert the strings to floats
 puts "float: #{(num1.to_f + num2.to_f)}"
